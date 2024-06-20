e0ec8824e3effda07f7c23327dc4db18e8119908
Boules Hisham <bolespolo4@gmail.com>
2024-06-20 15:07:25 -0700

    Add Number 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// Convert the first argument to an integer
+const x = parseInt(process.argv[2]);
+
+// Check if x is NaN or less than 1
+if (isNaN(x) || x < 1) {
+  console.log('Missing number of occurrences');
+} else {
+  // Loop to print "C is fun" x times
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
