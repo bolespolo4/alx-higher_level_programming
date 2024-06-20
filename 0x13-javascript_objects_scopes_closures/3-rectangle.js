729622ad8d3500c270af54bd8d12d8a9f86daefc
Boules Hisham <bolespolo4@gmail.com>
2024-06-20 12:40:08 -0700

    Edit 3 Rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,14 +1,31 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
+    // Constructor
+    constructor(w, h) {
+        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+            // If either width or height is not a positive integer, create an empty object
+            return {};  // Return an empty object
+        }
+        
+        // Initialize instance attributes
+        this.width = w;
+        this.height = h;
     }
-  }
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+
+    // Instance method to print the rectangle
+    print() {
+        if (!this.width || !this.height) {
+            console.log("Empty Rectangle");
+            return;
+        }
+
+        // Print the rectangle using 'X'
+        for (let i = 0; i < this.height; i++) {
+            let row = '';
+            for (let j = 0; j < this.width; j++) {
+                row += 'X';
+            }
+            console.log(row);
+        }
     }
-  }
 }
