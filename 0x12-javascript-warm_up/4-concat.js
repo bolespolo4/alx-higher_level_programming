af83e720ab6296ada7047044163f4b0418a88da0
Boules Hisham <bolespolo4@gmail.com>
2024-06-20 13:49:06 -0700

    Add 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,5 +2,5 @@
 if (process.argv[2] === undefined || process.argv[3] === undefined) {
     console.log('Missing arguments');
 } else {
-    console.log('${process.argv[2]} is ${process.argv[3]}');
+    console.log(`${process.argv[2]} is ${process.argv[3]}`);
 }
