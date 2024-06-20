1dcca52ba4b5bdc491df91421710e8e584c2a944
Boules Hisham <bolespolo4@gmail.com>
2024-06-20 13:39:37 -0700

    Modify File 2--

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,10 +3,15 @@
 // Check the number of arguments passed (excluding the first two default arguments)
 const argsCount = process.argv.length - 2;
 
-if (argsCount  === 0) {
+if (argsCount  === 0) 
+{
 	console.log('No argument');
-} else if (argsCount === 1) {
+} 
+else if (argsCount === 1) 
+{
 	console.log('Argument found');
-} else {
+} 
+else 
+{
 	console.log('Arguments found');
 }
