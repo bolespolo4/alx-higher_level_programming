f3196dbbb7d9ab0fa3452267b001367778c61967
Boules Hisham <bolespolo4@gmail.com>
2024-06-20 17:18:05 -0700

    fix code 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,31 +1,31 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-    // Constructor
-    constructor(w, h) {
-        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-            // If either width or height is not a positive integer, create an empty object
-            return {};  // Return an empty object
-        }
-        
-        // Initialize instance attributes
-        this.width = w;
-        this.height = h;
+  // Constructor
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      // If either width or height is not a positive integer, create an empty object
+      return {}; // Return an empty object
     }
 
-    // Instance method to print the rectangle
-    print() {
-        if (!this.width || !this.height) {
-            console.log("Empty Rectangle");
-            return;
-        }
+    // Initialize instance attributes
+    this.width = w;
+    this.height = h;
+  }
 
-        // Print the rectangle using 'X'
-        for (let i = 0; i < this.height; i++) {
-            let row = '';
-            for (let j = 0; j < this.width; j++) {
-                row += 'X';
-            }
-            console.log(row);
-        }
+  // Instance method to print the rectangle
+  print () {
+    if (!this.width || !this.height) {
+      console.log('Empty Rectangle');
+      return;
     }
-}
+
+    // Print the rectangle using 'X'
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+};
